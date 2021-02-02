72f02b48542bb4c50e1943f9eaae1b11915655d9
Echo-Mr-Pengw <2943927631@qq.com>
2021-02-02 20:46:37 +0800

    9.回文数

--- simple/isPalindrome.php
+++ simple/isPalindrome.php
@@ -9,7 +9,31 @@
  */
 
 
-function isPalindrome($x)
+function isPalindrome(string $x)
 {
+    $int = intval($x);
+	if ($int < 0) {
+		return false;
+	}
 
+    if ($int === 0 || $int < 10) {
+        return true;
+    }
+
+	$endPtr = strlen($x);
+    $midPtr = ceil($endPtr / 2);
+
+    // 前后取值比较
+	for ($i = 0; $i < $endPtr; $i++) {
+	    if ($x{$i} != $x{$endPtr - $i - 1}) {
+	        return false;
+        }
+        // 对折比较完，则退出
+	    if ($midPtr == $i + 1) {
+	        break;
+        }
+    }
+    return true;
 }
+$x = '121';
+var_dump(isPalindrome($x));

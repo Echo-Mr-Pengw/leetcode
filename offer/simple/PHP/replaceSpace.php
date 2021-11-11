2e63a3286c397df7c7c7d55c17bdb36e0fddc33b
pengwei5 <2943927631@qq.com>
2021-11-11 19:33:39 +0800

    优化

--- offer/simple/PHP/replaceSpace.php
+++ offer/simple/PHP/replaceSpace.php
@@ -22,3 +22,34 @@ function replaceSpace($s) {
    }
    return $data;
 }
+
+// 方法2 优化版
+function replaceSpaceOptimize($s) {
+
+    if (empty($s)) {
+        return $s;
+    }
+
+    $spaceNum = 0;
+    $len = strlen($s);
+    for ($i = 0; $i < $len; $i++) {
+        if ($s[$i] == ' ') {
+            $spaceNum++;
+        }
+    }
+
+    $allLen = $len + 2 * $spaceNum;
+
+    $i = $len - 1;
+    while ($i >= 0) {
+        if ($s[$i] == ' ') {
+            $s[--$allLen] = '0';
+            $s[--$allLen] = '2';
+            $s[--$allLen] = '%';
+        } else {
+            $s[--$allLen] = $s[$i];
+        }
+        $i--;
+    }
+    return $s;
+}
